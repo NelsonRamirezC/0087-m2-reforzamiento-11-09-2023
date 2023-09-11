d3e68c36080c504fff41941f768ba664ac99b2dd
Nelson Ramirez <xhyskoax@gmail.com>
2023-09-11 14:14:15 -0300

    Se agrega librería de gráficos CANVASJs

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,70 @@ $(()=> {
             //ASIGNAMOS EL RESULTADO DE LA CONVERSIÓN AL SPAN DEL DOM
             let resultadoConversion = $("#resultadoConversion");
             resultadoConversion.text(resultado);
+
+            //INVOCAMOS LA FUNCIÓN QUE BUSCA EL HISTORICO DE LA DIVISA
+            consultarHistorico(selectorIndicador);
         })
 
 
     })
+
+
+
+    //CONSULTAR HISTORICO DE LA DIVISA
+
+    function consultarHistorico(divisa){
+
+        let urlApi = "https://mindicador.cl/api/"+divisa;
+        fetch(urlApi)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let arraySerie = data.serie;
+
+            let dataPointsGrafico = arraySerie.map(serie => {
+                let objeto = {
+                    x: new Date(serie.fecha),
+                    y: serie.valor
+                }
+                return objeto
+            })
+
+            /* { x: 1501048673000, y: 35.939 } */
+            cargarGrafico(dataPointsGrafico);
+        })
+
+    }
+
+
+    function cargarGrafico(valores){
+        console.log(valores)
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title: {
+                text: "Variación mensual divisa"
+            },
+            axisX: {
+                title: "Fecha"
+            },
+            axisY: {
+                title: "Valor",
+                suffix: " CLP",
+                includeZero: true
+            },
+            data: [{
+                type: "line",
+                name: "Variación mensual DIVISA",
+                connectNullData: true,
+                //nullDataLineDashType: "solid",
+                xValueType: "dateTime",
+                xValueFormatString: "DD MMM",
+                yValueFormatString: "#,##0.##\" CLP\"",
+                dataPoints: valores
+            }]
+        });
+        chart.render();
+    }
 
 })
